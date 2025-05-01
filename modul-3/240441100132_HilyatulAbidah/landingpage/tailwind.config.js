a74f626ff404b6a40dc31a845d90d0e0524beb56
hilya-23 <hilyatulabidah34@gmail.com>
2025-05-01 19:40:51 +0700

    240441100132_HILYATUL ABIDAH_AHMAD RIKHAN ARBAI_MODUL 3

---
+++ modul-3/240441100132_HilyatulAbidah/landingpage/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html", // Pastikan semua file HTML dimasukkan ke dalam konfigurasi ini
+      "./src/**/*.{html,js}", // Jika kamu memiliki file HTML/JS lain
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
